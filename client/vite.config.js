9a8709039d45e9349db2523ecedca9e92715634c
EK.GD02 <ekkarat.thaitisa@gmail.com>
2025-12-02 06:02:11 +0700

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // บังคับให้ Polyfill ทำงานเพื่อแก้ปัญหา process และ util ของ simple-peer
       protocolImports: true,
     }),
   ],
+  // ✅ เพิ่มส่วนนี้เข้าไปเพื่อแก้ปัญหา simple-peer หา global ไม่เจอ
+  define: {
+    global: 'window', 
+  },
 })
